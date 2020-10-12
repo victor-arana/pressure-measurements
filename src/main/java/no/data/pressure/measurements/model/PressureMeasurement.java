6b2f196fb01272f564d72a893077495f3986a6de
victor-arana <victor_arana@hotmail.com>
2020-10-12 13:28:57 -0500

    :model::fields: add fields to model class
    Changes committed:
    	new file:   src/main/java/no/data/pressure/measurements/model/PressureMeasurement.java

---
+++ src/main/java/no/data/pressure/measurements/model/PressureMeasurement.java
@@ -0,0 +1,16 @@
+package no.data.pressure.measurements.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+public class PressureMeasurement {
+    private Integer systolicPressure;
+    private Integer diastolicPressure;
+    private Integer heartRate;
+}
