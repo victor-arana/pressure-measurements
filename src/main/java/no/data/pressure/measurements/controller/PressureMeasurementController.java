46d0cdb2ed5da77d7b96f9d91012fa922659fc5d
victor-arana <victor_arana@hotmail.com>
2020-10-12 13:40:52 -0500

    :controller::response: replace string response
    Changes committed:
    	modified:   src/main/java/no/data/pressure/measurements/controller/PressureMeasurementController.java

--- src/main/java/no/data/pressure/measurements/controller/PressureMeasurementController.java
+++ src/main/java/no/data/pressure/measurements/controller/PressureMeasurementController.java
@@ -1,11 +1,22 @@
 package no.data.pressure.measurements.controller;
 
+import no.data.pressure.measurements.model.PressureMeasurement;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
 
 @RestController
 public class PressureMeasurementController {
 
     @GetMapping("/pressureMeasurement")
-    public String getPressureMeasurements(){ return "pressureMeasurements";}
+    public PressureMeasurement getPressureMeasurements(){
+        PressureMeasurement pressureMeasurement = new PressureMeasurement();
+        pressureMeasurement.setId(1L);
+        pressureMeasurement.setDateTime(LocalDateTime.now());
+        pressureMeasurement.setSystolicPressure(130);
+        pressureMeasurement.setDiastolicPressure(80);
+        pressureMeasurement.setHeartRate(70);
+        return pressureMeasurement;
+    }
 }
