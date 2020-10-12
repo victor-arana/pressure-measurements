2fab7bc9d255dc9c3d8b4a536b72f0381d0db648
victor-arana <victor_arana@hotmail.com>
2020-10-12 11:15:42 -0500

    :configuration: generate project
    Generate project using start.spring.io using the following options:
    
    project: maven project
    language: java
    spring-boot: 2.3.4
    
    group: no.data
    artifact: pressure-measurements
    name: Blood Pressure Measurements
    package name: no.data
    packaging: jar
    java: 8
    
    dependencies: SpringWeb
    
    Changes committed:
    	new file:   .gitignore
    	new file:   pom.xml
    	new file:   src/main/java/no/data/pressure/measurements/BloodPressureMeasurementsApplication.java
    	new file:   src/main/resources/application.properties
    	new file:   src/test/java/no/data/pressure/measurements/BloodPressureMeasurementsApplicationTests.java

---
+++ src/main/java/no/data/pressure/measurements/BloodPressureMeasurementsApplication.java
@@ -0,0 +1,13 @@
+package no.data.pressure.measurements;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloodPressureMeasurementsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloodPressureMeasurementsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/no/data/pressure/measurements/BloodPressureMeasurementsApplicationTests.java
@@ -0,0 +1,13 @@
+package no.data.pressure.measurements;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloodPressureMeasurementsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
