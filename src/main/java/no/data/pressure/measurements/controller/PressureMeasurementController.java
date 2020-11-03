c33c8e8e93ab1fd39750ac93211c823bd06133fc
victor-arana <victor_arana@hotmail.com>
2020-11-03 11:44:50 -0600

    :controller::mapper::feature: use the mapper
    Changes committed:
    	modified:   src/main/java/no/data/pressure/measurements/controller/PressureMeasurementController.java

--- src/main/java/no/data/pressure/measurements/controller/PressureMeasurementController.java
+++ src/main/java/no/data/pressure/measurements/controller/PressureMeasurementController.java
@@ -1,22 +1,50 @@
 package no.data.pressure.measurements.controller;
 
+import no.data.pressure.measurements.dao.PressureMeasurementRepository;
+import no.data.pressure.measurements.entity.PressureMeasurementEntity;
 import no.data.pressure.measurements.model.PressureMeasurement;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class PressureMeasurementController {
 
-    @GetMapping("/pressureMeasurement")
-    public PressureMeasurement getPressureMeasurements(){
-        PressureMeasurement pressureMeasurement = new PressureMeasurement();
-        pressureMeasurement.setId(1L);
-        pressureMeasurement.setDateTime(LocalDateTime.now());
-        pressureMeasurement.setSystolicPressure(130);
-        pressureMeasurement.setDiastolicPressure(80);
-        pressureMeasurement.setHeartRate(70);
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @Autowired
+    private PressureMeasurementRepository repository;
+
+    @GetMapping("/pressureMeasurements")
+    public List<PressureMeasurement> getPressureMeasurements(){
+       List<PressureMeasurementEntity> pressureMeasurementEntities = repository.findAll();
+       return pressureMeasurementEntities.stream()
+               .map(this::mapToDto)
+               .collect(Collectors.toList());
+    }
+
+    @PostMapping("/pressureMeasurements")
+    PressureMeasurement create(@RequestBody PressureMeasurement pressureMeasurement){
+        PressureMeasurementEntity entity = mapToEntity(pressureMeasurement);
+        PressureMeasurementEntity savedEntity = repository.save(entity);
+        return mapToDto(savedEntity);
+    }
+
+    private PressureMeasurementEntity mapToEntity(PressureMeasurement pressureMeasurement) {
+        PressureMeasurementEntity pressureMeasurementEntity = modelMapper.map(pressureMeasurement, PressureMeasurementEntity.class);
+        return pressureMeasurementEntity;
+    }
+
+    private PressureMeasurement mapToDto(PressureMeasurementEntity pressureMeasurementEntity) {
+        PressureMeasurement pressureMeasurement = modelMapper.map(pressureMeasurementEntity, PressureMeasurement.class);
         return pressureMeasurement;
     }
 }
