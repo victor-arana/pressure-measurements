aa22b7115a460976484b4f5e3e90a78087c3d34d
victor-arana <victor_arana@hotmail.com>
2020-11-03 11:40:23 -0600

    :model::fields: add datetime field to model.
    Changes committed:
    	modified:   src/main/java/no/data/pressure/measurements/model/PressureMeasurement.java

--- src/main/java/no/data/pressure/measurements/model/PressureMeasurement.java
+++ src/main/java/no/data/pressure/measurements/model/PressureMeasurement.java
@@ -13,4 +13,5 @@ public class PressureMeasurement {
     private Integer systolicPressure;
     private Integer diastolicPressure;
     private Integer heartRate;
+    private LocalDateTime dateTime;
 }
